4a60e97f2d94bf245f059a72e2d8fb4fd9508127
xs2hamzashah <xs2hamzashah@gmail.com>
2024-10-08 00:03:52 +0500

    destroy method added in UserProfileViewSet to delete user along with UserProfile

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .models import UserProfile
 from .serializers import UserProfileSerializer
@@ -20,4 +21,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = UserProfileFilter  # Use the custom filter class
 
+    def destroy(self, request, *args, **kwargs):
+        # Get the UserProfile instance
+        user_profile = self.get_object()
 
+        # Get the related User instance
+        user = user_profile.user
+
+        # Delete the UserProfile and the related User
+        self.perform_destroy(user_profile)
+        user.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
