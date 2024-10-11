5317e644da86ed53aca5739e63f64eed6ce13442
xs2hamzashah <xs2hamzashah@gmail.com>
2024-10-12 00:52:40 +0500

    refactor(auth): renamed token endpoints for better clarity

--- cosmic_server7/urls.py
+++ cosmic_server7/urls.py
@@ -22,14 +22,14 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/auth/refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/verify-token/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/listings/', include('listings.urls')),
     path('api/operations/', include('operations.urls')),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
