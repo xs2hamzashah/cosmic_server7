bfb987e5d60b00a2211b53c7b0c350e214b269d2
xs2hamzashah <xs2hamzashah@gmail.com>
2024-10-15 00:25:58 +0500

    modifed recipient_list in the send_approval_notification

--- core/utils.py
+++ core/utils.py
@@ -6,7 +6,7 @@ from cosmic_server7.settings import EMAIL_HOST_USER
 def send_approval_notification(seller):
     subject = 'Your Solar Solution Has Been Approved'
     message = 'Congratulations! Your solar solution has been approved and is now live on the platform.'
-    # recipient_list = [seller.email]
-    recipient_list = []
+    recipient_list = [seller.email]
+    # recipient_list = []
 
     send_mail(subject, message, EMAIL_HOST_USER, recipient_list)
