e5061ffca757b8cc0bf6eef923e721ef3d627310
xs2hamzashah <xs2hamzashah@gmail.com>
2024-10-14 23:47:21 +0500

    modified default page size to 50

--- cosmic_server7/settings.py
+++ cosmic_server7/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,  # Number of items per page
+    'PAGE_SIZE': 10,  # Number of items per page
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
 }
