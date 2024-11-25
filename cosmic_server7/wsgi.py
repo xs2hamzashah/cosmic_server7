0683a184ee643494e3d9a44c53959e1031c65346
Hamza Shah <xs2hamzashah@gmail.com>
2024-11-26 00:17:29 +0500

    call migrate in the wsgi.py

--- cosmic_server7/wsgi.py
+++ cosmic_server7/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cosmic_server7.settings')
+
+call_command('migrate')
 
 application = get_wsgi_application()
