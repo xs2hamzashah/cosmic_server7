b24a97fd117a26c26cea2c98144cd2493e19c5b0
Hamza Shah <xs2hamzashah@gmail.com>
2024-12-10 15:47:24 +0500

    added views for pricelist models

--- pricelist/urls.py
+++ pricelist/urls.py
@@ -1,15 +1,31 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import (
+    PanelViewSet,
+    MechanicalWorkViewSet,
+    AfterSalesServiceViewSet,
+    BmsViewSet,
+    CivilWorkViewSet,
+    DcEarthingViewSet,
+    ElectricWorkViewSet,
+    HseEquipmentViewSet,
+    InverterViewSet,
+    BatteryViewSet,
+    NetMeteringViewSet,
+    OnlineMonitoringViewSet
+)
 
-from pricelist.views import PanelViewSet
-
-
-# Create a router and register the viewset
 router = DefaultRouter()
 router.register(r'panel', PanelViewSet, basename='panel')
+router.register(r'mechanical-work', MechanicalWorkViewSet, basename='mechanical-work')
+router.register(r'after-sales-service', AfterSalesServiceViewSet, basename='after-sales-service')
+router.register(r'bms', BmsViewSet, basename='bms')
+router.register(r'civil-work', CivilWorkViewSet, basename='civil-work')
+router.register(r'dc-earthing', DcEarthingViewSet, basename='dc-earthing')
+router.register(r'electric-work', ElectricWorkViewSet, basename='electric-work')
+router.register(r'hse-equipment', HseEquipmentViewSet, basename='hse-equipment')
+router.register(r'inverter', InverterViewSet, basename='inverter')
+router.register(r'battery', BatteryViewSet, basename='battery')
+router.register(r'net-metering', NetMeteringViewSet, basename='net-metering')
+router.register(r'online-monitoring', OnlineMonitoringViewSet, basename='online-monitoring')
 
-
-# The API URLs are now determined automatically by the router
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
