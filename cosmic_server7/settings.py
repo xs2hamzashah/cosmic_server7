b2ad5274a4a9cb993fc57152660f2eff03dc7b1a
Hamza Shah <xs2hamzashah@gmail.com>
2024-12-19 11:35:48 +0500

    modify DEFAULT_RENDERER_CLASSES in settings

--- cosmic_server7/settings.py
+++ cosmic_server7/settings.py
@@ -54,6 +54,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,  # Number of items per page
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ] if not DEBUG else [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
 
 }
 
