bcd4db1a93673f1c943083419e7caa3118157716
Hamza Shah <xs2hamzashah@gmail.com>
2024-12-29 14:28:26 +0500

    changed config name

--- cosmic_server7/settings_prod.py
+++ cosmic_server7/settings_prod.py
@@ -3,7 +3,7 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
+        'NAME': config('PRODUCTION_DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
